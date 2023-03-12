f6a900b5d4815467c195fdc6ef4deccf7ddfade4
kyli <ldh71777@gmail.com>
2023-03-12 15:27:01 +0800

    feat: amd

--- rspack.config.js
+++ rspack.config.js
@@ -3,8 +3,20 @@
  */
 module.exports = {
   context: __dirname,
+  mode: "development",
   entry: {
     main: "./src/main.jsx",
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  externalsType: "amd",
+  output: {
+    library: {
+      type: "amd",
+      name: "@[name]",
+    },
   },
   builtins: {
     html: [
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, "./dist_webpack"),
     library: {
       name: "@[name]",
-      type: "umd",
+      type: "amd",
     },
   },
   plugins: [new HtmlWebpackPlugin()],
