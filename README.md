10b3c8fa9a919cf97ea62b029cb45493c1b59ca1
kyli <ldh71777@gmail.com>
2023-03-12 15:17:21 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,21 @@
+This is a demo project to experience the differences between rspack and webpack.
+
+## install
+
+```sh
+pnpm install
+```
+
+## build
+
+rspack:
+
+```sh
+pnpm build
+```
+
+webpack:
+
+```sh
+pnpm wbuild
+```
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/main.jsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist_webpack"),
+    library: {
+      name: "@[name]",
+      type: "umd",
+    },
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  externals: ["react", "react-dom"],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: "css-loader",
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] },
+      },
+      {
+        test: /\.svg$/,
+        loader: "@svgr/webpack",
+      },
+    ],
+  },
+};
